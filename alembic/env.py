a653eba56ed2ab431fb6cd5dfe2ee8a3bdadbd51
Haley Morgan <haleymorgan3264@gmail.com>
2023-10-22 21:32:01 -0600

    redo db migrations

--- alembic/env.py
+++ alembic/env.py
@@ -1,9 +1,7 @@
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
-from tenk_nlp.db import DSN, engine
-from tenk_nlp.db.models import Base
+from tenk_nlp.db import DSN, engine, metadata
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,7 +17,7 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 # target_metadata = None
-target_metadata = [Base.metadata]
+target_metadata = [metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
--- tenk_nlp/db/__init__.py
+++ tenk_nlp/db/__init__.py
@@ -1,7 +1,11 @@
 import os
+from datetime import datetime
 
 from dotenv import load_dotenv
-from sqlalchemy import URL, create_engine
+from loguru import logger
+from sqlalchemy import URL, MetaData, create_engine, insert, select, update
+from sqlalchemy.dialects.mysql import insert as myinsert
+from sqlalchemy.orm.exc import NoResultFound
 
 load_dotenv()
 
@@ -15,10 +19,11 @@ db_config = {
 }
 
 if db_config["driver"] == "sqlite":
+    USE_SQLITE = True
     DSN = f"{db_config['driver']}:////{db_config['host']}"
 
 else:
-
+    USE_SQLITE = False
     DSN = URL.create(
         os.getenv("DB_DRIVER"),
         username=os.getenv("DB_USERNAME"),
@@ -27,4 +32,50 @@ else:
         database=os.getenv("DB_NAME"),
     )
 
-engine = create_engine(DSN, echo=True)
+engine = create_engine(DSN)
+
+metadata = MetaData()
+metadata.reflect(engine)
+
+
+class DataBase:
+    def execute_query(self, query_stmt):
+        with engine.begin() as conn:
+            result = conn.execute(query_stmt)
+            return result
+
+    def upsert_company(self, company_data: dict[str, str | int | datetime]):
+        table = metadata.tables["company"]
+
+        # upsert syntax diff for mysql vs sqlite
+        if USE_SQLITE:
+            result = self._sqlite_upsert(table, company_data)
+
+        else:
+            stmt = myinsert(table).values(company_data)
+            stmt = stmt.on_duplicate_key_update(company_data)
+            result = self.execute_query(stmt)
+        return result
+
+    def _sqlite_upsert(self, table, data):
+
+        # find if row exists with this id
+        data_id = data["id"]
+        find_stmt = select(table.c.id).where(table.c.id == data_id)
+        try:
+            row = self.execute_query(find_stmt).one()
+            # now we know a row exists so update the values
+            logger.debug(
+                f"[{table.name}] Item with id {data_id} already exists, updating"
+            )
+            update_stmt = update(table).values(data).where(table.c.id == data_id)
+            result = self.execute_query(update_stmt)
+        except NoResultFound:
+            # this means it's a new row, insert
+            insert_stmt = insert(table).values(data)
+            result = self.execute_query(insert_stmt)
+
+        return result
+
+
+database = DataBase()
